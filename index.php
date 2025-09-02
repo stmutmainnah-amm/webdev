6b7cd6be1ee0b0fdd3ddeb75cb612068296357b5
siti mutmainnah <smutmainnah020627@gmail.com>
2025-09-02 19:05:27 +0800

    index.php

--- index.php
+++ index.php
@@ -16,17 +16,27 @@ function mergeArrays(&$nums1, $m, $nums2, $n) {
     }
 }
 
+// Example 1
 $nums1 = [1,2,3,0,0,0];
 $m = 3;
 $nums2 = [2,5,6];
 $n = 3;
-
 mergeArrays($nums1, $m, $nums2, $n);
+echo "Example 1: [" . implode(", ", $nums1) . "]<br>";
 
-echo "<h2>Output:</h2>";
-echo "<pre>";
-echo "[" . implode(", ", $nums1) . "]";
-echo "</pre>";
+// Example 2
+$nums1 = [1];
+$m = 1;
+$nums2 = [];
+$n = 0;
+mergeArrays($nums1, $m, $nums2, $n);
+echo "Example 2: [" . implode(", ", $nums1) . "]<br>";
+
+// Example 3
+$nums1 = [0];
+$m = 0;
+$nums2 = [1];
+$n = 1;
+mergeArrays($nums1, $m, $nums2, $n);
+echo "Example 3: [" . implode(", ", $nums1) . "]<br>";
 ?>
-
-
